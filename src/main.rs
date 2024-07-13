b8f36089b70980ab579a59bc3a3e05dac2c91afd
Pedro Leão <pedroforestileao@gmail.com>
2024-07-13 03:10:07 -0300

    feature: added read file feature

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +8,9 @@ fn main() {
     let file_path = &args[2];
 
     println!("Searching for {} in {}", query, file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect(format!("Something went wrong reading the file at {}", file_path).as_str());
+
+    println!("With text:\n{}", contents);
 }
